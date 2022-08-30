842a66b2df65f678d8889dc3e3490d91d9ccd0df
shaunvan119 <shaunvan@live.com.au>
2022-08-30 20:26:25 +1000

    grid pics overlay

--- src/components/GridPics.js
+++ src/components/GridPics.js
@@ -16,20 +16,39 @@ export default function GridPics() {
     <div class="container">
         <img className="grid-image" src={gymone} alt="Logo" />
         <div class="overlay">
-        <div class="text">Hello World</div>
+        <div class="text">We love being part of a online fit community</div>
       </div>
   </div>
   <div class="container">
       <img className="grid-image grid-image-right" src={gymtwo} alt="Logo" />
       <div class="overlay overlay-right">
-      <div class="text">Hello World</div>
+      <div class="text">Online support pushed me to the next level</div>
     </div>
     </div>
+     <div class="container">
     <img className="grid-image grid-image-left" src={gymthree} alt="Logo" />
+    <div class="overlay overlay-left">
+      <div class="text">The online diet plans gave me structure</div>
+    </div>
+    </div>
+    <div class="container">
     <img className="grid-image " src={gymfour} alt="Logo" />
+    <div class="overlay overlay-right-two">
+      <div class="text">I enjoy the online community chat for tips</div>
+    </div>
+    </div>
+    <div class="container">
     <img className="grid-image grid-image-left" src={gymfive} alt="Logo" />
+    <div class="overlay overlay-left-two">
+      <div class="text">We love the gym based online team workouts</div>
+    </div>
+    </div>
+    <div class="container">
     <img className="grid-image" src={gymsix} alt="Logo" />
-    
+    <div class="overlay overlay-right-three">
+      <div class="text">The online programs are easy to follow</div>
+    </div>
+   </div>
     </div>
   );
 }
